9e3f0c6da2d57fd81225b5c24ce1ce95321d59f6
vonSchalienR <vonschalienrosa@gmail.com>
2024-01-17 22:47:26 +0200

    Update testi.java

--- Ex1/testi.java
+++ Ex1/testi.java
@@ -3,5 +3,7 @@ package Ex1;
 public class testi {
     public static void main(String[]args) {
         System.out.println("Line");
+        System.out.println("Line");
+        System.out.println("Line");
     }
 }
