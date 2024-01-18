fa4e400065dabb09afd83e0749a0e5a6d997f98c
vonSchalienR <vonschalienrosa@gmail.com>
2024-01-18 14:58:28 +0200

    Java KT1

---
+++ Ex1/KT1.java
@@ -0,0 +1,18 @@
+package Ex1;
+
+public class KT1 {
+    public static void main(String[]args){
+
+// Tehtävä 1 lausekkeet ja arvot ohjelmoimalla kohta A
+
+int a = 10;
+boolean tulos = a > 10;
+System.out.println("a > 10 : "+ tulos);
+
+    }
+}    
+    
+
+
+
+
---
+++ Ex1/KT1B.java
@@ -0,0 +1,15 @@
+package Ex1;
+
+public class KT1B {
+    public static void main(String[]args){    
+
+int a = 0;
+int b = 0;
+boolean tulos = a > b;
+System.out.println("a < b : "+ tulos);
+
+    }
+
+}
+    
+
