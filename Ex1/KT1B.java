2847e081cb612618fd73ecc2b15eeb3b058f4025
vonSchalienR <vonschalienrosa@gmail.com>
2024-01-19 11:27:37 +0200

    Update KT1B.java

--- Ex1/KT1B.java
+++ Ex1/KT1B.java
@@ -3,8 +3,10 @@ package Ex1;
 public class KT1B {
     public static void main(String[]args){    
 
-int a = 0;
-int b = 0;
+// Tehtävä 1 yhtälöiden laskeminen ohjelmalla osa B        
+
+int a = 15;
+int b = 2;
 boolean tulos = a > b;
 System.out.println("a < b : "+ tulos);
 
