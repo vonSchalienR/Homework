85555cd20b3309c50cfd086e7b1632cafa442568
vonSchalienR <vonschalienrosa@gmail.com>
2024-01-19 12:25:37 +0200

    .

---
+++ Ex1/KT1F.java
@@ -0,0 +1,13 @@
+package Ex1;
+
+public class KT1F {
+    public static void main(String[]args){
+
+        int a = 45;
+        int b = 69;
+        boolean tulos =	a * b > 0;
+        System.out.println(tulos);
+        
+    }
+    
+}
