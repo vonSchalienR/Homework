367d6f16fdf7b2daa30592b1d9b794b514c17984
vonSchalienR <vonschalienrosa@gmail.com>
2024-02-05 13:44:45 +0200

    Update KT4_1.java

--- Ex4/KT4_1.java
+++ Ex4/KT4_1.java
@@ -1,8 +1,18 @@
 package Ex4;
 
+import java.util.Scanner;
 public class KT4_1 {
+
+    private static final Scanner lukija = new Scanner(System.in);
     public static void main(String[]args){
         
+        System.out.println("Syötä luku:");
+        int luku1=lukija.nextInt();
+        System.out.println("Syötä toinen luku:");
+        int luku2=lukija.nextInt();
+        
+
+
     }
     
 }
