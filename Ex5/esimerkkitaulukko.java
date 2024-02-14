30c75f1900611868806d511a78fe2bb9f12322db
vonSchalienR <vonschalienrosa@gmail.com>
2024-02-14 14:14:54 +0200

    Update esimerkkitaulukko.java

--- Ex5/esimerkkitaulukko.java
+++ Ex5/esimerkkitaulukko.java
@@ -1,7 +1,33 @@
 package Ex5;
 
+import java.util.Scanner;
+
 public class esimerkkitaulukko {
+
+    private static final Scanner lukija = new Scanner(System.in);
     public static void main(String[] args) {
+
+        int[] luvut = {4,7,12,10,15,8,-5,7};
+        int[] taulu;
+
+        System.out.println("Luvut taulukon indeksissä 2 olevan alkion arvo");
+        System.out.println(luvut[2]);
+
+        System.out.println("Koko taulukko:");
+        for (int i = 0; i < luvut.length; i++){
+        System.out.println(luvut[1]+" ");
+        }
+        System.out.println("\n");
+    
+        taulu = luvut;
+        taulu [1] = luvut[0];
+        System.out.println(taulu);
+
+        String[] mjonot = new String[5];
+        mjonot[0]= "kukka";
+        mjonot[2]= "kakku";
+        mjonot[4]= "kukkia";
+        
         
     }
     
