0df5f1b348dfeafff8ad774f8b07d704193498fa
vonSchalienR <vonschalienrosa@gmail.com>
2024-03-19 12:39:15 +0200

    Create KT7_1.java

---
+++ Ex7/KT7_1.java
@@ -0,0 +1,12 @@
+package Ex7;
+
+import java.util.Scanner;
+
+public class KT7_1 {
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+
+    
+    }
+}
